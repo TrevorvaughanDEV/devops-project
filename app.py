2788c036c40c661a2f25bc798d6e53a5a99e963b
TrevorvaughanDEV <trevorvaughan120@gmail.com>
2026-03-24 11:46:41 +0000

    add ci pipeline

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Hello GitHub")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <html>
+        <head>
+            <title>DevOps App</title>
+        </head>
+        <body style="font-family: Arial; text-align:center; margin-top:50px;">
+            <h1>🚀 DevOps Project Live</h1>
+            <p>Deployed using Docker + AWS</p>
+            <p><b>Built by Trevor Vaughan</b></p>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
