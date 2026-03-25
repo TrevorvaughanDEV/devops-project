98df9d55fba7410e7a55695b90301f5509229a33
Trevor Vaughan <trevorvaughan120@gmail.com>
2026-03-25 18:10:44 +0000

    fix flask templates

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <html>
-        <head>
-            <title>DevOps App</title>
-        </head>
-        <body style="font-family: Arial; text-align:center; margin-top:50px;">
-            <h1>🚀 DevOps Project Live</h1>
-            <p>Deployed using Docker + AWS</p>
-            <p><b>Built by Trevor Vaughan</b></p>
-        </body>
-    </html>
-    """
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
